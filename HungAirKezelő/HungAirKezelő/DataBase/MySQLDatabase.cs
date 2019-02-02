aac3faed15832a4e7efd09b1b4d246f71dd0042d
Márkó Csernik <markoqw@hotmail.com>
2019-02-02 13:46:35 +0100

    Database mappa létrehozása, MySQLDatabase.cs és MySQLDatabaseInterface.cs hozzáadása

---
+++ HungAirKezelő/HungAirKezelő/DataBase/MySQLDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConnectToMysqlDatabase
+{
+    class MySQLDatabase
+    {
+        public MySQLDatabaseInterface getDatabaseInterface()
+        {
+            MySQLDatabaseInterface mdi = new MySQLDatabaseInterface();
+            mdi.setErrorToUserInterface(true);
+            mdi.setErrorToGraphicalUserInterface(false);
+            mdi.setConnectionServerData("localhost", "pizza","3306");
+            mdi.setConnectionUserData("root", "");
+            mdi.makeConnectionToDatabase();        
+
+            return mdi;
+        }
+    }
+}
